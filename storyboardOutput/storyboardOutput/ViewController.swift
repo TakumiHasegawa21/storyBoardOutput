f2758a02a82ec41d5eabdf7dfeede950af54397d
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2024-01-09 14:27:41 +0900

    ベースプロジェクトの作成

---
+++ storyboardOutput/storyboardOutput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storyboardOutput
+//
+//  Created by 長谷川拓海 on 2024/01/09.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
