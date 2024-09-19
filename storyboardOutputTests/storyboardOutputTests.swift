13bc3cda3b3673da3ef48621c58d732de32f9f19
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2024-09-19 13:09:16 +0900

    テストターゲットの追加

---
+++ storyboardOutputTests/storyboardOutputTests.swift
@@ -0,0 +1,36 @@
+//
+//  storyboardOutputTests.swift
+//  storyboardOutput
+//
+//  Created by 長谷川 拓海(Takumi Hasegawa) on 2024/09/19.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import XCTest
+
+final class storyboardOutputTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
